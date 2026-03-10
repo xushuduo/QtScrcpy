6e28f3468724aaf404147c17d03cbcfdc1e3f473
XuShuduo <xushuduo@XuShuduodeMac-mini.local>
2026-03-11 02:26:52 +0800

    ui

--- QtScrcpy/ui/toolform.cpp
+++ QtScrcpy/ui/toolform.cpp
@@ -48,11 +48,11 @@ void ToolForm::initStyle()
     IconHelper::Instance()->SetIcon(ui->openScreenBtn, QChar(0xf06e), 15);
     IconHelper::Instance()->SetIcon(ui->closeScreenBtn, QChar(0xf070), 15);
     IconHelper::Instance()->SetIcon(ui->powerBtn, QChar(0xf011), 15);
-    // IconHelper::Instance()->SetIcon(ui->expandNotifyBtn, QChar(0xf103), 15);
+    IconHelper::Instance()->SetIcon(ui->expandNotifyBtn, QChar(0xf103), 15);
     IconHelper::Instance()->SetIcon(ui->screenShotBtn, QChar(0xf0c4), 15);
-    // IconHelper::Instance()->SetIcon(ui->touchBtn, QChar(0xf111), 15);
+    IconHelper::Instance()->SetIcon(ui->touchBtn, QChar(0xf111), 15);
     IconHelper::Instance()->SetIcon(ui->groupControlBtn, QChar(0xf0c0), 15);
-    // IconHelper::Instance()->SetIcon(ui->clipboardBtn, QChar(0xf0c5), 15);
+    IconHelper::Instance()->SetIcon(ui->clipboardBtn, QChar(0xf0c5), 15);
 }
 
 void ToolForm::updateGroupControl()
