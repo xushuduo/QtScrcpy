865a9ec15cc605324cea2f50e410ced0b6a1f753
XuShuduo <xushuduo@XuShuduodeMac-mini.local>
2026-03-21 01:57:47 +0800

    fix power-off-on-close

--- QtScrcpy/util/config.h
+++ QtScrcpy/util/config.h
@@ -20,6 +20,7 @@ struct UserBootConfig
     bool autoOffScreen    = false;
     bool framelessWindow  = false;
     bool keepAlive        = false;
+    bool powerOffOnClose  = true;
     bool simpleMode       = false;
     bool autoUpdateDevice = true;
     bool showToolbar      = true;
